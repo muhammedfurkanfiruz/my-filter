557be9c3338fcd397a4a85f68d1d39d49b6dd3a0
Furkan <mfurkan.firuz@gmail.com>
2022-08-12 11:18:53 +0300

    creating data table and  binding  with json file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import "./App.css";
+import { useState } from "react";
+import "./App.css"
+import {Users} from "./users"
+import Table from "./Table";
 
 function App() {
-  return <div className="App">
-    deneme
-  </div>;
+  const [query, setQuery] = useState();
+  const Search = (data) => {
+    return data.filter((item) =>item.first_name.toLowerCase().includes(query));
+  }
+    return (
+    <div className="app" >
+
+    <pre> {JSON.stringify({query},null,2)}</pre>
+
+        <input type="text" placeholder="Search.." className="search" onChange={(e) => setQuery(e.target.value)}/>
+        <Table data = {Search(Users)} />
+        </div>
+
+    );
 }
 
 export default App;
